dcc110af029b3d9465d0a51738919660ddd1745c
EdipoBorges <edipokong@hotmail.com>
2024-11-19 23:40:00 -0300

    test1

--- core/settings.py
+++ core/settings.py
@@ -77,8 +77,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
